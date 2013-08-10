c38930479544e662c838a8e951ebb451f912cab9
sween <sween@Rons-MacBook-Pro.local>
2013-08-09 23:42:19 -0400

    Added a cutesy message.

--- app.rb
+++ app.rb
@@ -16,33 +16,12 @@ configure do
   enable :sessions
 end
 
-#helpers do
-#  def username
-#    session[:identity] ? session[:identity] : 'Hello stranger'
-#  end
-#end
-
 
 get '/' do
   erb :human_api
 end
 
 
-#post '/login/attempt' do
-#  session[:identity] = params['username']
-#  where_user_came_from = session[:previous_url] || '/'
-#  redirect to where_user_came_from 
-#end
-
-#get '/logout' do
-#  session.delete(:identity)
-#  erb "<div class='alert alert-message'>Logged out</div>"
-#end
-
-
-# get '/secure/place' do
-#  erb "This is a secret place that only <%=session[:identity]%> has access to!"
-# end
 
 get '/auth/humanapi/callback' do
   auth   = env['omniauth.auth']
@@ -50,8 +29,7 @@ get '/auth/humanapi/callback' do
   email  = auth[:info][:email]
   token  = auth[:credentials][:token]
   
-  HumanAPI::Human.token = token
-  
-  HumanAPI::Human.profile['email']
+  HumanAPI::Human.token = token 
+  HumanAPI::Human.profile['email'] + " <---was pulled from the HumanAPI"
   
 end
