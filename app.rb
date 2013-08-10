f1f6dab6b43fae941fc595b2e9c7d0c5320890fa
sween <sween@Rons-MacBook-Pro.local>
2013-08-09 22:32:55 -0400

    Some Clean Up

--- app.rb
+++ app.rb
@@ -15,48 +15,38 @@ configure do
   enable :sessions
 end
 
-helpers do
-  def username
-    session[:identity] ? session[:identity] : 'Hello stranger'
-  end
-end
+#helpers do
+#  def username
+#    session[:identity] ? session[:identity] : 'Hello stranger'
+#  end
+#end
 
-before '/secure/*' do
-  if !session[:identity] then
-    session[:previous_url] = request.path
-    @error = 'Sorry guacamole, you need to be logged in to visit ' + request.path
-    halt erb(:login_form)
-  end
-end
 
 get '/' do
   erb :human_api
 end
 
-get '/login/form' do 
-  erb :login_form
-end
 
-post '/login/attempt' do
-  session[:identity] = params['username']
-  where_user_came_from = session[:previous_url] || '/'
-  redirect to where_user_came_from 
-end
+#post '/login/attempt' do
+#  session[:identity] = params['username']
+#  where_user_came_from = session[:previous_url] || '/'
+#  redirect to where_user_came_from 
+#end
 
-get '/logout' do
-  session.delete(:identity)
-  erb "<div class='alert alert-message'>Logged out</div>"
-end
+#get '/logout' do
+#  session.delete(:identity)
+#  erb "<div class='alert alert-message'>Logged out</div>"
+#end
 
 
-get '/secure/place' do
-  erb "This is a secret place that only <%=session[:identity]%> has access to!"
-end
+# get '/secure/place' do
+#  erb "This is a secret place that only <%=session[:identity]%> has access to!"
+# end
 
 get '/auth/humanapi/callback' do
   auth   = env['omniauth.auth']
   uid    = auth[:uid]
   email  = auth[:info][:email]
   token  = auth[:credentials][:token]
-  'ok'
+  uid
 end
